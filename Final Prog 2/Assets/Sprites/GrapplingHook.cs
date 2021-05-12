6339b920959167a2fc2335a543ba81f22996fa95
DESKTOP-3235VA4\harry <yg1766@nyu.edu>
2021-05-13 07:51:37 +0800

    Grappling Hook 2.0 Test

---
+++ Final Prog 2/Assets/Sprites/GrapplingHook.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+    Vector2 startPoint;
+    Vector2 endPoint;
+
+    Vector2 grapplePoint;
+    private GameObject player;
+    public Camera mainCam;
+    private LineRenderer lR;
+    private DistanceJoint2D dJ;
+    
+    void Start()
+    {
+        player = GameObject.Find("PlayerPlaceHolder");
+        lR = player.GetComponent<LineRenderer>();
+        dJ = player.GetComponent<DistanceJoint2D>();
+        disableGrapple();
+    }
+
+
+    void Update()
+    {
+
+        startPoint = player.transform.position;
+        endPoint = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        if (!Input.GetMouseButton(0))
+        {
+            grapplePoint = endPoint;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            lR.SetPosition(0,startPoint);
+            lR.SetPosition(1,grapplePoint);
+            enableGrapple();
+            
+
+        }
+        else
+        {
+            lR.SetPosition(0,new Vector2(0,0));
+            lR.SetPosition(1,new Vector2(0,0));
+            disableGrapple();
+        }
+
+    }
+
+    public void enableGrapple()
+    {
+        dJ.connectedAnchor = grapplePoint;
+        dJ.enabled = true;
+    }
+    
+    public void disableGrapple()
+    {
+        dJ.enabled = false;
+    }
+}
---
+++ Final Prog 2/Assets/Sprites/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float forceScale;
+
+    public float jumpHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * forceScale, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector2.right * forceScale, ForceMode2D.Force);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+}
